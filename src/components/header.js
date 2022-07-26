1c1386da3c5a02194ae5679504f0ed5f31b9686d
saurabhspatil <saurabhspatil99@gmail.com>
2022-07-26 16:03:44 +0530

    Removing console

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,6 @@ import React from 'react';
 import '../App.css';
 export default function AppHeader(props){
     const pathName = window.location.pathname;
-    console.log(pathName)
     const headerLabel = pathName.includes('view-coin-details') ? 'Crypto Coin Details' : 'Crypto Coins';
     return (
         <>
